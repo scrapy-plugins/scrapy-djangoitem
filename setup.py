bf3db4c1d0e5fdae0c696536c39b8df6f2a5b49c
Julia Medina <who.knows.shia@gmail.com>
2015-04-18 13:20:16 -0300

    Additional files to complete the package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='scrapy-djangoitem',
+    version='0.0.0',
+    url='https://github.com/scrapy/scrapy-djangoitem',
+    description='Scrapy extension to write scraped items using Django models',
+    long_description=open('README.rst').read(),
+    author='Scrapy developers',
+    license='BSD',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Framework :: Scrapy',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'Scrapy>=0.24.5',
+        'Django',
+    ],
+)
